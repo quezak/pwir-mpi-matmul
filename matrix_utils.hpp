9be8fb750b8cc78c4996a7a348ac6d5e06828895
Artur Kozak <artur.kozak2@gmail.com>
2016-05-26 12:28:15 +0200

    add abstract matrix interface

---
+++ matrix_utils.hpp
@@ -0,0 +1,44 @@
+#ifndef MATRIX_UTILS_HPP
+#define MATRIX_UTILS_HPP
+#include <string>
+#include <functional>
+
+/// Abstract interface for both dense and sparse matrices.
+class Matrix {
+public:
+    const int height, width;
+
+    Matrix(int h, int w): height(h), width(w) {}
+
+    /// Return value at given coordinates.
+    virtual double& at(int row, int col) = 0;
+    virtual const double& at(int row, int col) const = 0;
+
+    /// Set value at given coordinates.
+    virtual void set(int row, int col, double value) = 0;
+
+    class Row {
+        Matrix *m;
+        const int index;
+    public:
+        Row(Matrix *_m, int row): m(_m), index(row) {}
+        double& operator[](int col) {
+            return m->at(index, col);
+        }
+    };
+
+    /// Syntactic sugar: m[row][col] = v should run set(row, col).
+    // note: this probably should hava a const version, but who cares
+    virtual Row operator[](int row) {
+        return Row(this, row);
+    }
+
+    /// Return matrix slice containing rows [start, end)
+    virtual Matrix& getRowBlock(int start, int end) const = 0;
+
+    /// Return matrix slice containing columns [start, end)
+    virtual Matrix& getColBlock(int start, int end) const = 0;
+
+};
+
+#endif  // MATRIX_UTILS_HPP
